619af7c0b981a863f2d38eac6595a93d32106a5a
Bader <10670@holbertonstudents.com>
2025-02-14 17:18:48 +0000

    update

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,6 @@
+/*
+ *hello
+ */
 #include <stdio.h>
 /*
  *return alaways 0
@@ -5,6 +8,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-  /*print with puts */
 	return (0);
 }
