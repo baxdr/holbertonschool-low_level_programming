4799eb02f1d5676d4715c6267efdca3bad2246e8
Bader <10670@holbertonstudents.com>
2025-02-14 16:30:59 +0000

    update

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,7 +4,8 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+  //print by puts
+  puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
