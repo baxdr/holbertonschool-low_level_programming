7eeac7d4ace253e53ba07c7c2be53bdf196b163c
Bader <10670@holbertonstudents.com>
2025-02-14 17:25:02 +0000

    update

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/*
+ *file 5-printf.c
+ */
+#include <stdio.h>
+/**
+ * print with printf
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return(0);
+}
