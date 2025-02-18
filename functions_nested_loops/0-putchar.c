fe1ed972682d1268d9c9821d0c298a8c7197356b
Bader <10670@holbertonstudents.com>
2025-02-18 16:43:40 +0000

    update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - entry point
- *
- * Return: alway returns 0
- *
- */
+* main - entry point
+*
+* Return: alway returns 0
+*
+*/
 int main(void)
 {
-	char word[8]="_putchar";
-	for(int i = 0 ; i < word ; i++ )
-		_putchar(word[i]);
-	_putchar('\n');
-	return (0);
+int i;
+char word[8] = "_putchar";
+for (i = 0 ; i < 8 ; i++)
+{
+_putchar(word[i]);
+}
+_putchar('\n');
+return (0);
 }
