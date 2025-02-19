b063aa176b81d1d5af104570b47bd0980ed949f5
Bader <10670@holbertonstudents.com>
2025-02-19 14:49:31 +0000

    update

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * int _isalpha - check code
- * dis : 1 for letter 
+ * _isalpha - Check Holberton
+ * @c: An input character
+ * Description: function returns 1 if the character is a
+ * letter, lowercase or uppercase.
+ * Return: 1 or 0 in otherwise.
  */
 int _isalpha(int c)
 {
-	if (c <= 122 && c >=97)
+	if (c <= 122 && c >= 97)
 		return (1);
-	else if(c <= 90 && c >=65)
+	else if (c <= 90 && c >= 65)
 		return (1);
 	else
 		return (0);
