4cc799f26afbd0490a3fdc2c40416f972c4cad74
Bader <10670@holbertonstudents.com>
2025-02-19 13:58:54 +0000

    update

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _islower - check the code.
- *
+ * @c: an input ch
  * Return: Always 0.
  */
 int _islower(int c)
 {
-	if (c >95)
-		return 1;
-	else 
-		return 0;
+	if (c > 95)
+		return (1);
+else
+		return (0);
 }
