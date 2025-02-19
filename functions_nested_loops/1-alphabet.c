329763dc5694f73588c09091ab336b7cf2e83dd7
Bader <10670@holbertonstudents.com>
2025-02-19 12:54:44 +0000

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,8 +4,11 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
-    return (0);
+	char ch;
+	for (ch = 'a' ;ch <= 'z' ; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
 }
