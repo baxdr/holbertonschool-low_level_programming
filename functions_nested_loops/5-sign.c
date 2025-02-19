a3349ce91fb56a4baf42e64af9c56ce6d9807964
Bader <10670@holbertonstudents.com>
2025-02-19 15:39:00 +0000

    update

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,16 +9,23 @@
 
 int print_sign(int n)
 {
+
 	int i = 0;
 
 	if (n > 0)
+	{
 		_putchar('+');
 		i = 1;
+	}
 	else if (n < 0)
+	{
 		_putchar('-');
 		i = -1;
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
 		i = 0;
+	}
 	return (i);
 }
