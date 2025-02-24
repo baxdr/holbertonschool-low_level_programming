ad5debba4bac3a0ef38f291477131d4245d46244
Bader <10670@holbertonstudents.com>
2025-02-24 16:17:40 +0000

    update

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
