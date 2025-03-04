b0a930c6c417fd399d0b40a3db4aa938997d0072
Bader <10670@holbertonstudents.com>
2025-03-04 20:50:58 +0000

    update

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return (NULL);
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
