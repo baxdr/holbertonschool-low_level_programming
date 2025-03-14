7696652cc671f26dc34a638223cf8d679f9a8623
Bader <10670@holbertonstudents.com>
2025-03-14 17:20:40 +0000

    update

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,9 +13,12 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
+
 	if (array == NULL)
 		return (NULL);
-	for (index = 0 ; index < size ; index++)
+
+	for (index = 0; index < size; index++)
 		array[index] = c;
+
 	return (array);
 }
