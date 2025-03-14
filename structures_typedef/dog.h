b061e34457efdb1a80b1a4924ace18fbe229bf16
Bader <10670@holbertonstudents.com>
2025-03-14 18:12:37 +0000

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog with a name, age, and owner
+ * @name: Pointer to the dog's name (string)
+ * @age: The dog's age (float)
+ * @owner: Pointer to the owner's name (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif /* DOG_H */
