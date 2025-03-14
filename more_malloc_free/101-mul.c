40aceb4bfe119bc270e692c983115600d99d0446
Bader <10670@holbertonstudents.com>
2025-03-14 17:54:06 +0000

    update

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -11,7 +11,7 @@
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+    return (c >= '0' && c <= '9');
 }
 
 /**
@@ -22,11 +22,10 @@ int _isdigit(int c)
  */
 int _strlen(char *s)
 {
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
+    int i = 0;
+    while (*s++)
+        i++;
+    return (i);
 }
 
 /**
@@ -38,47 +37,51 @@ int _strlen(char *s)
  */
 char *big_multiply(char *s1, char *s2)
 {
-	char *r;
-	int l1, l2, a, b, c, x;
+    char *r;
+    int l1, l2, a, b, c, x;
 
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
+    l1 = _strlen(s1);
+    l2 = _strlen(s2);
+    r = malloc(a = x = l1 + l2);
+    if (!r)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    while (a--)
+        r[a] = 0;
 
-	for (l1--; l1 >= 0; l1--)
-	{
-		if (!_isdigit(s1[l1]))
-		{
-			free(r);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[l1] - '0';
-		c = 0;
+    for (l1--; l1 >= 0; l1--)
+    {
+        if (!_isdigit(s1[l1]))
+        {
+            free(r);
+            printf("Error\n");
+            exit(98);
+        }
+        a = s1[l1] - '0';
+        c = 0;
 
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
+        for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+        {
+            if (!_isdigit(s2[l2]))
+            {
+                free(r);
+                printf("Error\n");
+                exit(98);
+            }
+            b = s2[l2] - '0';
 
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
+            c += r[l1 + l2 + 1] + (a * b);
+            r[l1 + l2 + 1] = c % 10;
 
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
-	}
-	return (r);
+            c /= 10;
+        }
+        if (c)
+            r[l1 + l2 + 1] += c;
+    }
+    return (r);
 }
-
 
 /**
  * main - multiply two big number strings
@@ -89,27 +92,35 @@ char *big_multiply(char *s1, char *s2)
  */
 int main(int argc, char **argv)
 {
-	char *r;
-	int a, c, x;
+    char *r;
+    int a, c, x;
 
-	if (argc != 3)
-		printf("Error\n"), exit(98);
+    if (argc != 3)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
-	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(r);
-	return (0);
+    x = _strlen(argv[1]) + _strlen(argv[2]);
+    r = big_multiply(argv[1], argv[2]);
+    
+    /* Print the result */
+    c = 0;
+    a = 0;
+    while (c < x)
+    {
+        if (r[c])
+            a = 1;
+        if (a)
+            _putchar(r[c] + '0');
+        c++;
+    }
+    if (!a)
+        _putchar('0');
+    _putchar('\n');
+
+    /* Free allocated memory */
+    free(r);
+    return (0);
 }
+
