915c8e27adaf0d5dcd6215b28caa5eddfb6546aa
Bader <10670@holbertonstudents.com>
2025-03-14 17:18:07 +0000

    update

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_array - func create array
+ * @size: size of array
+ * @c: value
+ * Return: null if 0 or fails or array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	*array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+	for (index = 0 ; index < size ; index++)
+		array[index] = c;
+	return (array);
+}
