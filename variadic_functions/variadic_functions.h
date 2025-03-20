58bc14f32bc3f646a9740be56f3558f47f5b7f4d
Bader <10670@holbertonstudents.com>
2025-03-20 21:33:51 +0000

    update

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,29 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int print_c(va_list a);
+int print_i(va_list a);
+int print_f(va_list a);
+int print_s(va_list a);
 /**
  * struct op - struct for 3
  * @c: character to check
  * @f: function to point to
  */
-#include <stdarg.h>
-#include <stdio.h>
-
-typedef struct printer
+typedef struct op
 {
-char *symbol;
-void (*print)(va_list arg);
-}
-printer_t;
-
-
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
+	char *c;
+	int (*f)(va_list a);
+} printer;
 #endif
