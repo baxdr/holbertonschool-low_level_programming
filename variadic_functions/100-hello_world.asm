3d057a67db5fa3f2afeec770f6d56845bfc13bcd
Bader <10670@holbertonstudents.com>
2025-03-20 22:01:01 +0000

    update

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -2,16 +2,16 @@ global main
 
 section .text
 main:
-	mov rax, 1; write(
-	mov rdi, 1; STDOUT_FILENO,
-	mov rsi, msg; "Hello, Holberton\n",
-	mov rdx, msglen ; sizeof("Hello, Holberton\n")
-	syscall ; );
+    mov rax, 1        ; syscall number for sys_write
+    mov rdi, 1        ; file descriptor 1 (stdout)
+    mov rsi, msg      ; pointer to the message
+    mov rdx, msglen   ; message length
+    syscall           ; invoke system call
 
-	mov rax, 60 ; exit(
-	mov rdi, 0; EXIT_SUCCESS
-	syscall ; );
+    mov rax, 60       ; syscall number for sys_exit
+    xor rdi, rdi      ; status 0 (EXIT_SUCCESS)
+    syscall           ; invoke system call
 
 section .data
-	msg: db "Hello, Holberton", 10
-	msglen: equ $ - msg
+msg: db "Hello, World", 10
+msglen: equ $ - msg
