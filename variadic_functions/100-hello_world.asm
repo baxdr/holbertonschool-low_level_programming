b4152c8a9f72772a87af39e2adf1a8a7a61aa5d6
Bader <10670@holbertonstudents.com>
2025-03-20 21:50:38 +0000

    update

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,17 +1,17 @@
+global main
 
-	global main
-
-	section .text
+section .text
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, message
-	mov rdx, 17
-	syscall
+	mov rax, 1; write(
+	mov rdi, 1; STDOUT_FILENO,
+	mov rsi, msg; "Hello, Holberton\n",
+	mov rdx, msglen ; sizeof("Hello, Holberton\n")
+	syscall ; );
 
-	mov eax, 60
-	xor rdi, rdi
-	syscall
+	mov rax, 60 ; exit(
+	mov rdi, 0; EXIT_SUCCESS
+	syscall ; );
 
-message:
-	db "Hello, Holberton", 10
+section .data
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
