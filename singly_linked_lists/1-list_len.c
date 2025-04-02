af8ceed20a13d53bed61b9a7eaf0e8ee5f3fe89e
Bader <10670@holbertonstudents.com>
2025-04-02 05:56:05 +0000

    task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - number of element
+ * @h: list
+ * Return: return number of element
+ */
+size_t list_len(const list_t *h)
+{
+	size count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
